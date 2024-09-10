d4056e2cebebe940bf6401088ac96e8265c9a597
ddg999 <slowman918@gmail.com>
2024-09-10 17:36:25 +0900

    Feat : 관리자 대출현황 목록 및 연체로직

--- src/main/java/com/library/bookwave/repository/interfaces/AdminRepository.java
+++ src/main/java/com/library/bookwave/repository/interfaces/AdminRepository.java
@@ -3,8 +3,8 @@ package com.library.bookwave.repository.interfaces;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
+import com.library.bookwave.repository.model.Lend;
 import com.library.bookwave.repository.model.User;
 
 @Mapper
@@ -12,4 +12,8 @@ public interface AdminRepository {
 	public int insert(User user);
 
 	public List<User> readAllUser();
+	
+	public List<Lend> readAllLend();
+	
+	public int updateLendStatus();
 }
--- src/main/java/com/library/bookwave/service/AdminService.java
+++ src/main/java/com/library/bookwave/service/AdminService.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.library.bookwave.repository.interfaces.AdminRepository;
+import com.library.bookwave.repository.model.Lend;
 import com.library.bookwave.repository.model.User;
 
 import lombok.RequiredArgsConstructor;
@@ -37,6 +39,39 @@ public class AdminService {
 		List<User> userList = new ArrayList<>();
 		userList = adminRepository.readAllUser();
 		return userList;
+	}
+
+	@Transactional
+	public List<Lend> readAllLend() {
+		List<Lend> lendList = new ArrayList<>();
+		lendList = adminRepository.readAllLend();
+		return lendList;
+	}
+
+	//	// 매 5초마다 실행
+	//	@Scheduled(fixedRate = 5000)
+	//	public void executeTask() {
+	//		System.out.println("5초마다 실행되는 작업");
+	//	}
+
+	// 매일 자정에 실행
+	@Scheduled(cron = "0 0 0 * * *")
+	@Transactional
+	public void updateLendStatusAtMidnight() {
+		System.out.println("---------------------------");
+		System.out.println("자정에 실행");
+		System.out.println("---------------------------");
+		int result = 0;
+		try {
+			result = adminRepository.updateLendStatus();
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (result != 1) {
+			System.out.println("업데이트 할 데이터가 없습니다");
+		}
 	}
 
 }
